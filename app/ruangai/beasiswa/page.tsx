811b67c1f06b5ced2ccfa652672f3930f86e6c5e
azhardanii <azhardanii@yahoo.com>
2025-08-23 18:39:39 +0700

    add logo sekolah wfa at form page & update layout

--- app/ruangai/beasiswa/page.tsx
+++ app/ruangai/beasiswa/page.tsx
@@ -1,12 +1,12 @@
 "use client";
 
 import React, { useRef, useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function RuangAIPage() {
   const iframeRef = useRef<HTMLIFrameElement>(null);
   const [isLoading, setIsLoading] = useState(true);
-  const router = useRouter();
 
   useEffect(() => {
     const iframe = iframeRef.current;
@@ -23,14 +23,6 @@ export default function RuangAIPage() {
   return (
     <div className="flex flex-col h-screen w-screen overflow-hidden bg-gray-100 font-inter">
       <div className="relative flex-grow">
-        {/* 🔘 Button langsung push ke /ruangai */}
-        <button
-          onClick={() => router.push("/ruangai")}
-          className="fixed top-4 right-4 z-20 px-4 py-2 bg-emerald-600 text-white font-semibold rounded-lg shadow-lg hover:bg-emerald-700 transition"
-        >
-          Kembali
-        </button>
-
         {isLoading && (
           <div className="absolute inset-0 flex items-center justify-center bg-gray-200 z-10 rounded-lg">
             <div className="animate-spin rounded-full h-10 w-10 border-4 border-b-emerald-500 border-gray-400"></div>
@@ -38,13 +30,27 @@ export default function RuangAIPage() {
           </div>
         )}
 
+        <Link href="/ruangai" className="absolute top-0 left-0 w-full bg-emerald-500 text-white text-center p-1.5 font-semibold hover:bg-emerald-600 transition-colors z-50 cursor-pointer">
+          ✨ Detail Info tentang RuangAI, <span className="underline font-bold tracking-wide">Klik di Sini!!</span> ✨
+        </Link>
+        <div className="absolute top-0 w-full h-[6.75rem] bg-[#C9E6F0]" />
+        <div className="absolute top-12 w-full flex justify-center">
+          <Image 
+            src="/wfaxruangai.webp" 
+            width={300} 
+            height={150} 
+            alt="Logo Sekolah WFA X RuangAI"
+            priority
+          />
+        </div>
+
         <iframe
           ref={iframeRef}
           id="ruang-ai-iframe"
           src="https://www.ruangai.id/registration?ref=38C5EC"
           title="Sekolah WFA x Ruang AI Codepolitan"
           sandbox="allow-scripts allow-same-origin allow-popups allow-forms allow-pointer-lock"
-          className={`w-full h-full border-0 transition-opacity duration-500 rounded-lg shadow-xl ${
+          className={`w-full h-full border-0 transition-opacity duration-500 rounded-lg shadow-xl scrollbar-hide ${
             isLoading ? "opacity-0" : "opacity-100"
           }`}
           onLoad={() => setIsLoading(false)}

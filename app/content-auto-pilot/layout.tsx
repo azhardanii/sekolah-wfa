f1cf43b50b49575e755ea39245c008cadef87b1e
azhardanii <azhardanii@yahoo.com>
2025-10-31 15:56:36 +0700

    fix: add meta pixel

--- app/content-auto-pilot/layout.tsx
+++ app/content-auto-pilot/layout.tsx
@@ -1,6 +1,7 @@
 import "../globals.css";
 import { Poppins } from "next/font/google";
 import type { Metadata } from "next";
+import Script from "next/script";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -51,6 +52,23 @@ export default function ContentAutoPilotLayout({
     <main
       className={`min-h-screen bg-white relative overflow-x-hidden [&::-webkit-scrollbar]:w-1.5 [&::-webkit-scrollbar-track]:bg-transparent [&::-webkit-scrollbar-thumb]:bg-white/30 hover:[&::-webkit-scrollbar-thumb]:bg-white/50 [&::-webkit-scrollbar-thumb]:rounded-full ${poppins.className}`}
     >
+      <Script id="meta-pixel" strategy="afterInteractive">
+        {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '1346212237107053');
+          fbq('track', 'PageView');
+        `}
+      </Script>
+      <noscript><img height="1" width="1" style={{ display: "none" }}
+        src="https://www.facebook.com/tr?id=1346212237107053&ev=PageView&noscript=1" />
+      </noscript>
       {children}
     </main>
   );
